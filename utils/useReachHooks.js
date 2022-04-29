f167d1e99f1b03a45083102c346171ca8f7c9b0f
Nithiwat Seesillapachai <nithiwatter@gmail.com>
2022-04-28 23:05:53 -0400

    add more comments

--- utils/useReachHooks.js
+++ utils/useReachHooks.js
@@ -11,7 +11,12 @@ function useReachPopoverMenu() {
       // copy all details of native mousedown event
       // except isTrusted - readonly property from dispatchEvent()
       // browser heuristics would somehow make this apply focus-visible (no way out?) - inconsistent after some testing!
+
+      // basically, the any hovering (before the first manual click on the button) is treated as a keyboard input -> focus-visible menu
+      // after the first manual click, all later hoverings are treated as mouse clicks -> not focus-visible menu
+      // no way around this unless could somehow modify isTrusted readonly property to true?
       bubbles: true,
+      button: 0,
       buttons: 1,
       cancelable: true,
       detail: 1,
