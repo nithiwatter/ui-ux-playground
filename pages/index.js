61f75f8645d9572818fc27a5ab52c156ac83e26c
Nithiwat Seesillapachai <nithiwatter@gmail.com>
2022-04-29 20:38:02 -0400

    test downshift/tabbing between menus

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-import CohereMenu from '../components/downshift/cohereMenu';
-// import CohereMenu from '../components/reach/cohereMenu';
+// import CohereMenu from '../components/downshift/cohereMenu';
+import CohereMenu from '../components/reach/cohereMenu';
 // import CohereMenu from '../components/radix/cohereMenu';
 
 export default function Home() {
   return (
     <div className="bg-slate-800 p-8">
-      <CohereMenu />
+      <div className="flex gap-4">
+        <CohereMenu />
+        <CohereMenu />
+      </div>
     </div>
   );
 }
