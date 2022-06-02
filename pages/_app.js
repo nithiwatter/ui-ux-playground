f45446dceba1ed0288e2a49951d3a258e7d53bfe
nithiwatter <nithiwatter@gmail.com>
2022-06-01 22:03:12 -0400

    test cache provider

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,16 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
 import '../styles/globals.css';
 
+import RestProvider from '../components/rest-hooks/RestProvider';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <RestProvider>
+        <Component {...pageProps} />
+      </RestProvider>
+    </>
+  );
 }
 
 export default MyApp;
