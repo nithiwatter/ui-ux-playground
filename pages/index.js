675c6661909793f39e1eb671939bc785b84422c7
nithiwatter <nithiwatter@gmail.com>
2022-12-23 04:26:56 +0700

    test

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // import CohereMenu from '../components/downshift/cohereMenu';
 import CohereMenu from '../components/reach/cohereMenu';
-// import CohereMenu from '../components/radix/cohereMenu';
+import CohereMenu from '../components/radix/cohereMenu';
 
 export default function Home() {
   return (
